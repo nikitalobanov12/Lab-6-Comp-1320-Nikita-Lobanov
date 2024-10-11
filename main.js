e1acb32f7c07d6d4ec78f51156d72c31e5ff3bc0
Nikita Lobanov <nikitaalobanovv@gmail.com>
2024-10-11 15:09:02 -0700

    Working Solution

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const { register, createABlog, createAPost, likePost } = require('./logic.js');
 
 
   const blogName = "Nikita's Blog";
-  const post = "blog post";
-  const username = "vinsent";
+  const post = "Blog post";
+  const username = "Nikita";
   
   register(username, "password")
-    // .then(() => createABlog(blogName))
+    .then(() => createABlog(blogName))
     .then(() => createAPost(post, "hello world", blogName))
     .then(() => likePost(blogName, post, username))
     .catch((error) => {
